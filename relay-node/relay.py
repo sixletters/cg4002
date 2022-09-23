1ea6f5c2f80f1907a66eaf05d818810e8fa2fe0f
harris maung <harrismaung3@gmail.com>
2022-09-24 00:46:34 +0800

    update 3

--- relay-node/relay.py
+++ relay-node/relay.py
@@ -1,22 +1,41 @@
 import socket
+
+from Ultra96-python.ultra import receiverProcess
 HOST = "0.0.0.0"
 MYTCP_PORT = 8080
-tempDataMap = {
-    "shield": "1",
-    "reload": "2",
-    "activate": "3",
-    "shoot": "4",
-    "exit": "5",
-    "grenade": "6"
-}
+import multiprocessing as mp
 
-if __name__ == '__main__':
+# tempDataMap = {
+#     "shield": "1",
+#     "reload": "2",
+#     "activate": "3",
+#     "shoot": "4",
+#     "exit": "5",
+#     "grenade": "6"
+# }
+def relayProcess(dataBuffer, lock):
     while True:
-        action = input("Enter the action")
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-            sock.connect((HOST, MYTCP_PORT))
-            encodedAction = tempDataMap[action].encode("utf-8")
-            sock.send(encodedAction)
-            receivedMsg = sock.recv(2048)
-            print(receivedMsg)
-            sock.close()
+        if not dataBuffer.empty():
+            action = dataBuffer.get()
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+                sock.connect((HOST, MYTCP_PORT))
+                encodedAction = action.serialise()
+                sock.send(encodedAction)
+                receivedMsg = sock.recv(2048)
+                print(receivedMsg)
+                sock.close()
+                
+def internalComms(dataBuffer,lock):
+    while True:
+        ## INTERNAL COMMS SHUD BE PUTTING THE STRUCTURES INTO THE DATABUFFER
+        print("INTERNAL COMMS STUFF")
+        
+if __name__ == '__main__':
+    dataBuffer = mp.Queue()
+    lock = mp.Lock()
+    relay = mp.Process(target=receiverProcess, args=(dataBuffer, lock))
+    internalComms = mp.process(target=internalComms, args=(dataBuffer, lock))
+    relay.start()
+    internalComms.start()
+    relay.join
+    internalComms.join
